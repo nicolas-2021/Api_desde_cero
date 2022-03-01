f81592435eacbb6c4905c1b409362bfad91764e9
nicolas-2021 <nicolasmartinamuedo1@gmail.com>
2022-03-01 19:42:25 -0300

    Se agregaron las rutas,y se comentaron porque falta la conexion con base de datos.Tambien se modificaron algunas res con json objects aqui y en index

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config(); // Agregamos el requerimiento del modulo dotenv
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/* var usersRouter = require('./routes/users.router');
+var productsRouter = require('.routes/products.router');
+var ordersRouter = require('./routes/orders.router');
+var authRouter = require('.routes/auth.router'); */
+//Agregamos las rutas que tenemos que crear en /routes
 
 var app = express();
 
@@ -20,22 +26,29 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/* app.use('/auth', authRouter);             
+app.use('/api/users', usersRouter);       
+app.use('/api/products', productsRouter);
+app.use('/api/orders', ordersRouter); */
+//Asignamos las rutas que usara la app
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler, esto sucede si intentamos acceder a rutas que no existen en las app.use
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404, 'The end point does not exist'));  //Le agregamos un mensaje al error 404 entre comillas
 });
 
-// error handler
+// error handler , esto le sigue al error 404
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the error page , Agregamos un error en formato json
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    errorcode: err.status || 500,
+    message: res.locals.message
+  });
 });
 
 module.exports = app;
