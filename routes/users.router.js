c7bd43819e59567a1d10fa5d2c2ee850167aedbd
nicolas-2021 <nicolasmartinamuedo1@gmail.com>
2022-03-24 20:38:08 -0300

    usamos extensiÓn de VsCode ThunderClient, para simular ser cliente y hacer pruebas//creamos jsonresponse modulo interno para manejo de aciertos vs createError para manejo de errores

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,5 +1,8 @@
 var express = require('express');
+const createError = require('http-errors');
 var router = express.Router();
+const {jsonresponse} = require('../lib/jsonresponse');
+
 const User= require('../model/users.model');
 
 
@@ -12,6 +15,28 @@ router.get('/', async function(req, res, next) {
 
   }
   res.json(results);
+});
+
+router.post('/', async function(req, res, next) {
+  const {username, password} = req.body;
+
+  if(!username || !password){
+    next(createError(400, 'El Usuario y la contraseña son requeridos'));
+  }else if(username && password){
+    const user = new User({username, password});
+
+    const exists = await user.usernameExists(username);
+
+    if(exists){
+      next(createError(400, 'El Usuario ya existe'));
+    }else{
+      await user.save();
+
+      res.json(jsonresponse(200, {
+        message: 'El Usuario fue registrado correctamente'
+      }));
+    }
+  }
 });
 
 module.exports = router;
